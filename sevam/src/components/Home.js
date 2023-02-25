003aafc058a029321784dec707e480d1df862b17
arunNewPhoenix <2245arunkumarz@gmail.com>
2023-02-25 22:09:07 +0530

    setup the frame

---
+++ sevam/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./navs/Nav";
+import Hording from "./hoarding/Hording";
+export function Home() {
+  return (
+   <div>
+   <Nav/>
+   <Hording/>
+   
+   </div>
+  )
+}
+
+
---
+++ sevam/src/components/hoarding/Hording.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './hoarding.css'
+const Hording = () => {
+  return (
+    <div>
+    <div className='banner'>
+
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Hording
